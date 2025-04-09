e4f196ea188df4f89ee1a9a8965003918fbf81da
Alex <av2o26@outlook.com>
2025-04-08 21:08:16 -0700

    commit

--- src/main/java/com/gcu/CloudApp.java
+++ src/main/java/com/gcu/CloudApp.java
@@ -11,7 +11,7 @@ public class CloudApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudApp.class, args);
-		log.info("Springboot logging has started.");
+		log.info("Springboot logging has begun.");
 	}
 
 }
